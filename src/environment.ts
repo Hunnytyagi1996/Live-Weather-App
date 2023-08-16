3cfee1c34d46960396b6d6a75aed60a794289816
Hunny Tyagi <hunnytyagi1996@gmail.com>
2023-08-16 11:55:50 -0400

    code push for app

---
+++ src/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-key',
+    XRapidAPIKeyHeaderValue: 'd8b1b33cb3msh42b55663f655e1cp17ddafjsnab6543860a20',
+};
